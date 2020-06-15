fada724b2e1fbc24b94d3a4e55f78f77fee1f1c3
adilgondal1 <adil.gondal@yale.edu>
2020-06-15 10:03:21 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    #attr_accessor :name, :day, :season, :genre
+
+    def actors_list
+        self.actors.map{|actor| actor.full_name}
+    end
   
 end
